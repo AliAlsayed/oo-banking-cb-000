7d544ada810be86f5815258a07b6ff159be76a27
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-02 06:18:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,4 +10,13 @@ class Transfer
   def valid?
     @sender.valid? && @receiver.valid?
   end
+
+  def execute_transaction
+    if @sender.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = 'complete'
+    end
+    "Transaction rejected. Please check your account balance."
+  end
 end
